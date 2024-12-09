354edf941bb30724f283130dceb2de0c14183973
unknown <b.camara.diaby@outlook.com>
2024-12-09 23:10:08 +0100

    modif

--- envoyer_contact.php
+++ envoyer_contact.php
@@ -11,45 +11,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $attachment = $_FILES['attachment'];
 
-    $mail = new PHPMailer(true);
+    $phpmailer = new PHPMailer(true);
 
     try {
         // Configuration du serveur SMTP
-        $mail->isSMTP();
-        $mail->Host = 'sandbox.smtp.mailtrap.io'; // Adresse du serveur SMTP de Mailtrap
-        $mail->SMTPAuth = true;
-        $mail->Username = '42e27e6fee9041'; // Votre nom d'utilisateur Mailtrap
-        $mail->Password = '76f234318a9081'; // Votre mot de passe Mailtrap
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 2525;
+        $phpmailer->isSMTP();
+        $phpmailer->Host = 'sandbox.smtp.mailtrap.io'; // Adresse du serveur SMTP de Mailtrap
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Username = '42e27e6fee9041'; // Votre nom d'utilisateur Mailtrap
+        $phpmailer->Password = '76f234318a9081'; // Votre mot de passe Mailtrap
+        $phpmailer->SMTPSecure = 'tls';
+        $phpmailer->Port = 2525;
 
         // En-têtes de l'e-mail
-        $mail->setFrom('from@example.com', 'Babou-CAMARA-DIABY');
-        $mail->addAddress('b.camara.diaby@outlook.com'); // Ajouter un destinataire
+        $phpmailer->setFrom('from@example.com', 'Babou-CAMARA-DIABY');
+        $phpmailer->addAddress('b.camara.diaby@outlook.com'); // Ajouter un destinataire
 
         // Ajouter la pièce jointe si elle existe
         if ($attachment['error'] == UPLOAD_ERR_OK) {
-            $mail->addAttachment($attachment['tmp_name'], $attachment['name']);
+            $phpmailer->addAttachment($attachment['tmp_name'], $attachment['name']);
         }
 
         // Contenu de l'e-mail
-        $mail->isHTML(false); // Envoyer l'e-mail en texte brut
-        $mail->Subject = "Ton profil intéresse " . $name;
-        $mail->Body    = "Nom: " . $name . "\nEmail: " . $email . "\nMessage: \n" . $message;
+        $phpmailer->isHTML(false); // Envoyer l'e-mail en texte brut
+        $phpmailer->Subject = "Ton profil intéresse " . $name;
+        $phpmailer->Body    = "Nom: " . $name . "\nEmail: " . $email . "\nMessage: \n" . $message;
 
         // Envoyer l'e-mail
-        $mail->send();
+        $phpmailer->send();
 
         // Envoyer l'e-mail de confirmation à l'utilisateur
-        $mail->clearAddresses();
-        $mail->addAddress($email);
-        $mail->Subject = "Confirmation de réception de votre message";
-        $mail->Body    = "Bonjour " . $name . ",\n\nMerci de votre intérêt pour mon profil. J'ai bien reçu votre message et je reviendrais vers vous dès que possible.\n\nCordialement,\nBabou CAMARA-DIABY";
-        $mail->send();
+        $phpmailer->clearAddresses();
+        $phpmailer->addAddress($email);
+        $phpmailer->Subject = "Confirmation de réception de votre message";
+        $phpmailer->Body    = "Bonjour " . $name . ",\n\nMerci de votre intérêt pour mon profil. J'ai bien reçu votre message et je reviendrais vers vous dès que possible.\n\nCordialement,\nBabou CAMARA-DIABY";
+        $phpmailer->send();
 
         echo "Votre message a bien été envoyé.</br>";
     } catch (Exception $e) {
-        echo "Votre message n'a pas été envoyé: {$mail->ErrorInfo}</br>";
+        echo "Votre message n'a pas été envoyé: {$phpmailer->ErrorInfo}</br>";
     }
 
     echo "Vous allez être redirigé à la page précédente dans 5 secondes...";
